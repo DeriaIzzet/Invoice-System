29d6a0acd572a05c6ecf3114d155544972f89a57
Deria Izzet <deriaizzzet@gmail.com>
2024-02-11 02:30:02 +0200

    add delete

--- invoices/resources/views/invoices/index.blade.php
+++ invoices/resources/views/invoices/index.blade.php
@@ -32,7 +32,7 @@
                                 <td>
                                     <a href="" class="btn btn-info btn-sm">View</a>
                                     <a href="{{ route('invoices.edit', $invoice) }}" class="btn btn-primary btn-sm">Edit</a>
-                                    <form action="" method="POST" style="display: inline;">
+                                    <form action="{{ route('invoices.destroy', $invoice->id) }}" method="POST" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this invoice?')">Delete</button>
